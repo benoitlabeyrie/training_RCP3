bed7d3720a2fd68abb0d5e5568fabaad03dfddf5
blabeyri <blabeyri@LFR011900.corp.capgemini.com>
2017-03-08 14:02:50 +0100

    labels avec couleur

--- com.sogeti.rental.ui/src/com/sogeti/rental/ui/RentalProvider.java
+++ com.sogeti.rental.ui/src/com/sogeti/rental/ui/RentalProvider.java
@@ -2,14 +2,19 @@ package com.sogeti.rental.ui;
 
 import java.util.Collection;
 
+import org.eclipse.jface.viewers.IColorProvider;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Display;
 
 import com.opcoach.training.rental.Customer;
+import com.opcoach.training.rental.Rental;
 import com.opcoach.training.rental.RentalAgency;
 import com.opcoach.training.rental.RentalObject;
 
-public class RentalProvider extends LabelProvider implements ITreeContentProvider{
+public class RentalProvider extends LabelProvider implements ITreeContentProvider, IColorProvider {
 
 	@Override
 	public Object[] getElements(Object inputElement) {
@@ -96,6 +101,28 @@ public class RentalProvider extends LabelProvider implements ITreeContentProvide
 			return label;
 		}
 		
+	}
+
+	@Override
+	public Color getForeground(Object element) {
+		if (element instanceof RentalAgency) {
+			return Display.getCurrent().getSystemColor(SWT.COLOR_BLUE);
+		} else if (element instanceof Customer) {
+			return Display.getCurrent().getSystemColor(SWT.COLOR_RED);
+		} else if (element instanceof Rental) {
+			return Display.getCurrent().getSystemColor(SWT.COLOR_GREEN);
+		} else if (element instanceof RentalObject) {
+			return Display.getCurrent().getSystemColor(SWT.COLOR_YELLOW);
+		} else {
+			return null;
+		}
+		
+	}
+
+	@Override
+	public Color getBackground(Object element) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
